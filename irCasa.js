0a329805e9a3087cfb1eb6f35521befd77ec0b08
Estef072 <61289015+Estef072@users.noreply.github.com>
2022-03-02 18:31:50 -0600

    Add files via upload

---
+++ irCasa.js
@@ -0,0 +1,30 @@
+const irCasa = document.createElement('irCasa');
+asesinosBuscados.innerHTML =  `
+        <head>
+        <title>Cada vez mas cerca</title>
+        </head>
+        <body>
+        <article>
+            <h2>Las flores</h2>
+            <p>
+                Cuando Estefan llega a realizar el cateo a casa de filip, nada de lo que habia dentro le permitia 
+                conecatar con Emily, hasta que dentro de la casa habia un ramo de flores no tan secas, es decir el
+                habia estado ahí hace poco, ademas eran las mismas flores que hay en el parque donde encontraron a 
+                Emily.
+                Por lo que el detective corre al parque.
+
+
+            </p>
+        </article>
+        <img src="fotos/flores.jpg" alt="flores" width="450" height="350">
+
+
+        <ul>
+            <li>
+                <a href="parque.html">Corre al parque</a>
+            </li>
+        
+        </ul>
+        </body>
+`
+document.body.appendChild(irCasa);
